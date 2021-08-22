4f9147130d6ad28a4e68d85575af3c71df13046c
skanderKhabou <skander.khabou@gmail.com>
2021-08-23 00:12:56 +0200

    injection automatique des dépendence grâce a l annotation autowire

--- src/main/java/com/mycompany/invoise/service/InvoiceServiceNumber.java
+++ src/main/java/com/mycompany/invoise/service/InvoiceServiceNumber.java
@@ -2,9 +2,11 @@ package com.mycompany.invoise.service;
 
 import com.mycompany.invoise.entity.Invoice;
 import com.mycompany.invoise.repository.InvoiceRepositoryInterface;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class InvoiceServiceNumber implements InvoiceServiceInterface{
     private static long lastNumber=0L;
+    @Autowired
     private InvoiceRepositoryInterface invoiceRepository;
 
 
--- src/main/java/com/mycompany/invoise/service/InvoiceServicePrefix.java
+++ src/main/java/com/mycompany/invoise/service/InvoiceServicePrefix.java
@@ -2,10 +2,19 @@ package com.mycompany.invoise.service;
 
 import com.mycompany.invoise.entity.Invoice;
 import com.mycompany.invoise.repository.InvoiceRepositoryInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.File;
 
 public class InvoiceServicePrefix implements InvoiceServiceInterface{
-    private static long lastNumber=112L;
+
+    private  long lastNumber;
+    private String prefix;
+    private File fichier;
+
+    @Autowired
     private InvoiceRepositoryInterface invoiceRepository;
+
 
 
 
@@ -18,7 +27,32 @@ public class InvoiceServicePrefix implements InvoiceServiceInterface{
     }
 
     public void createInvoice(Invoice invoice){
-        invoice.setNumber(String.valueOf("INV_" + (++lastNumber)));
+        invoice.setNumber(String.valueOf(prefix + (++lastNumber)));
         invoiceRepository.create(invoice);
+    }
+
+
+    public long getLastNumber() {
+        return lastNumber;
+    }
+
+    public void setLastNumber(long lastNumber) {
+        this.lastNumber = lastNumber;
+    }
+
+    public String getPrefix() {
+        return prefix;
+    }
+
+    public void setPrefix(String prefix) {
+        this.prefix = prefix;
+    }
+
+    public File getFichier() {
+        return fichier;
+    }
+
+    public void setFichier(File fichier) {
+        this.fichier = fichier;
     }
 }
